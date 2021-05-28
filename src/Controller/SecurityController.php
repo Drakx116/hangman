5bbaf3b1a1246950f6fc825d4e65a2d8f1879b96
Drakx116 <alexandre2.guyot@gmail.com>
2021-05-28 11:10:52 +0200

    Security - Handle user registration (with auto authentication)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use App\Security\AppUserAuthenticator;
+use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * @Route("/register", name="app_register")
+     * @throws ORMException
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $encoder, GuardAuthenticatorHandler $guardHandler, AppUserAuthenticator $authenticator, UserRepository $repository): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $plainPassword = $form->get('password')->getData();
+            $user->setPassword($encoder->encodePassword($user, $plainPassword));
+
+            $repository->persist($user);
+            $repository->flush();
+
+            return $guardHandler->authenticateUserAndHandleSuccess($user, $request, $authenticator, 'main');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
